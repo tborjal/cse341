45381d43e954f1af5b3a3610ebe92aae77be57af
tborjal <bor22014@byui.edu>
2024-06-12 17:44:27 +0800

    assignment 1

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+ 
+app.get('/', (req, res) => {
+  res.send("Ebak");
+});
+ 
+const port = 3000;
+app.listen(process.env.PORT || 3000, () => {
+  console.log('Web Server is listening at port ' + (process.env.PORT || 3000));
+});
